5763704a97a9364a5a0d675e756ab05372227fe7
raheshit <raheshinfo@gmail.com>
2019-03-23 10:02:12 +0430

    add sign in and sing up component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    SignInComponent,
+
   ],
   imports: [
     BrowserModule,
