eb2992bfdac1aec891ce87bb6e6f2791038f6c9d
raheshit <raheshinfo@gmail.com>
2019-03-23 10:26:13 +0430

    init sing in component

--- frontend/src/app/components/auth/sign-in/sign-in.component.ts
+++ frontend/src/app/components/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
+  user: User = new User();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(): void {
+    console.log(this.user);
   }
 
 }
