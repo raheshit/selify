978b8946c0cbdc08e695ae6721d9683790978cb4
raheshit <raheshinfo@gmail.com>
2019-03-24 10:38:54 +0430

    add router link to header html

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import  {SigninComponent} from  './components/auth/signin/signin.component';
 import  {SignupComponent} from "./components/auth/signup/signup.component";
-
 import {from} from "rxjs";
+
 const routes: Routes = [
   {
     path:'auth/signin',
