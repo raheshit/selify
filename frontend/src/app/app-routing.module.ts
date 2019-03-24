2b6f194a21ad057e898b2c5a46ffef6d92fd2932
raheshit <raheshinfo@gmail.com>
2019-03-24 10:16:24 +0430

    add signup and in to app-routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import  {SigninComponent} from  './components/auth/signin/signin.component';
+import  {SignupComponent} from "./components/auth/signup/signup.component";
 
-const routes: Routes = [];
+import {from} from "rxjs";
+const routes: Routes = [
+  {
+    path:'auth/signin',
+    component:SigninComponent
+  },
+  {
+    path:'auth/signup',
+    component:SignupComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
